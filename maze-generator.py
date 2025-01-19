7c43597989e9899aabeae28ecf59c6275dc9ea05
Joel Williams <jtw058@uark.edu>
2025-01-18 21:41:11 -0600

    minor formatting changes

--- maze-generator.py
+++ maze-generator.py
@@ -6,34 +6,36 @@ import random
 pygame.init()
 
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 750
-CELL_SIZE = 25
+SCREEN_HEIGHT = 800
+CELL_SIZE = 20
 N_COLS = SCREEN_WIDTH // CELL_SIZE
 N_ROWS = SCREEN_HEIGHT // CELL_SIZE
 
 COLORS = {
-    "BLACK":       ( 0,   0,   0   ),
-    "WHITE":       ( 255, 255, 255 ),
-    "RED":         ( 212, 57,  57  ),
-    "PURPLE":      ( 124, 57,  212 ),
-    "LIGHTPURPLE": ( 184, 158, 219 ),
-    "GRAYPURPLE":  ( 116, 98,  140 )
+    "BLACK":       (0,   0,   0),
+    "WHITE":       (255, 255, 255),
+    "RED":         (212, 57,  57),
+    "PURPLE":      (124, 57,  212),
+    "LIGHTPURPLE": (184, 158, 219),
+    "GRAYPURPLE":  (116, 98,  140)
 }
 
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Maze Generator")
 
 CLOCK = pygame.time.Clock()
-FPS = 60
+FPS = 15
+
 
 def index(i, j):
     return i * N_ROWS + j
+
 
 class Cell:
     def __init__(self, i, j):
         self.i = i
         self.j = j
-        self.walls = [True, True, True, True] # top, right, bottom, left
+        self.walls = [True, True, True, True]  # top, right, bottom, left
         self.visited = False
 
     def show(self):
@@ -41,17 +43,27 @@ class Cell:
 
         # draw fill color if cell is visited
         if self.visited:
-            pygame.draw.rect(SCREEN, COLORS["PURPLE"], (x, y, CELL_SIZE, CELL_SIZE))
+            pygame.draw.rect(SCREEN, COLORS["PURPLE"],
+                             (x, y, CELL_SIZE, CELL_SIZE))
 
         # draw cell walls
         if self.walls[0]:
-            pygame.draw.line(SCREEN, COLORS["WHITE"], (x, y), (x + CELL_SIZE, y), 2)
+            pygame.draw.line(SCREEN, COLORS["WHITE"],
+                             (x, y),
+                             (x + CELL_SIZE, y), 2)
         if self.walls[1]:
-            pygame.draw.line(SCREEN, COLORS["WHITE"], (x + CELL_SIZE, y), (x + CELL_SIZE, y + CELL_SIZE), 2)
+            pygame.draw.line(SCREEN, COLORS["WHITE"],
+                             (x + CELL_SIZE, y),
+                             (x + CELL_SIZE, y + CELL_SIZE), 2)
         if self.walls[2]:
-            pygame.draw.line(SCREEN, COLORS["WHITE"], (x, y + CELL_SIZE), (x + CELL_SIZE, y + CELL_SIZE), 2)
+            pygame.draw.line(SCREEN, COLORS["WHITE"],
+                             (x, y + CELL_SIZE),
+                             (x + CELL_SIZE, y + CELL_SIZE), 2)
         if self.walls[3]:
-            pygame.draw.line(SCREEN, COLORS["WHITE"], (x, y), (x, y + CELL_SIZE), 2)
+            pygame.draw.line(SCREEN, COLORS["WHITE"],
+                             (x, y),
+                             (x, y + CELL_SIZE), 2)
+
 
 class MazeGrid:
     def __init__(self):
@@ -69,7 +81,7 @@ class MazeGrid:
         # initialize grid of cells
         for i in range(N_COLS):
             for j in range(N_ROWS):
-                self.cells.append(Cell(i,j))
+                self.cells.append(Cell(i, j))
 
         # set top left cell as visited and add to stack
         self.cells[0].visited = True
@@ -84,20 +96,22 @@ class MazeGrid:
         if len(self.stack) > 0:
             cell = self.stack.pop()
             nextCell = self.checkCellNeighbors(cell.i, cell.j)
-            if nextCell != None:
+            if nextCell is not None:
                 self.stack.append(cell)
                 self.removeWalls(cell, nextCell)
                 nextCell.visited = True
                 self.stack.append(nextCell)
-            
+
             # mark the current cell with a red color
-            pygame.draw.rect(SCREEN, COLORS["RED"], (cell.i * CELL_SIZE, cell.j * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            pygame.draw.rect(SCREEN, COLORS["RED"],
+                             (cell.i * CELL_SIZE, cell.j * CELL_SIZE,
+                              CELL_SIZE, CELL_SIZE))
         else:
             self.finished = True
-    
+
     def checkCellNeighbors(self, i, j):
         unvisited = []
-        
+
         if j - 1 >= 0 and j - 1 < N_ROWS:
             top = self.cells[index(i, j - 1)]
             if not top.visited:
@@ -114,12 +128,12 @@ class MazeGrid:
             left = self.cells[index(i - 1, j)]
             if not left.visited:
                 unvisited.append(left)
-        
+
         if len(unvisited) > 0:
             return random.choice(unvisited)
         else:
             return None
-    
+
     def removeWalls(self, a, b):
         if a.i - b.i == 1:
             a.walls[3] = False
@@ -135,7 +149,6 @@ class MazeGrid:
             b.walls[0] = False
 
 
-
 if __name__ == "__main__":
     maze = MazeGrid()
     widgets = []
@@ -149,11 +162,11 @@ if __name__ == "__main__":
     # button to start a new maze
     newMazeButton = Button(
         SCREEN, SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT - 55, 200, 40,
-        text = "Start New Maze", fontSize = 20, margin = 10,
-        shadowDistance = 2, shadowColour = COLORS["BLACK"],
-        inactiveColour = COLORS["GRAYPURPLE"],
-        pressedColour = COLORS["LIGHTPURPLE"], radius = 15,
-        onRelease = reset
+        text="Start New Maze", fontSize=20, margin=10,
+        shadowDistance=2, shadowColour=COLORS["BLACK"],
+        inactiveColour=COLORS["GRAYPURPLE"],
+        pressedColour=COLORS["LIGHTPURPLE"], radius=15,
+        onRelease=reset
     )
     newMazeButton.hide()
     widgets.append(newMazeButton)
@@ -166,7 +179,7 @@ if __name__ == "__main__":
                 run = False
                 continue
         pw.update(events)
-        
+
         if not maze.finished:
             maze.update()
         else:
